7e5d1047b0fdf4a378962406d97a282ef76115aa
mjx666 <meijiaxin666@gmail.com>
2026-04-21 18:46:04 +0800

    git_learn

---
+++ input_learn.py
@@ -0,0 +1,2 @@
+user_id=input("please input your id:")
+print(f"user id is "+ user_id)
---
+++ python_test1.py
@@ -0,0 +1,16 @@
+import main
+import math
+print("hello"+" mjx")
+my_name="mjx666"
+print(my_name)
+num1=2^3 #^是按位异或符号
+num2=2**3 #**是次幂符号
+s="hello world"
+print(num1)
+print(main.num_sum(num1,num2))
+print(math.__doc__)
+help(math.pow)
+print(f"数字的类型为：",type(3.0))
+print(f"字符串长度为:",len(s))
+print(f"第三个字符是：",s[len(s)-8])
+print(f"字符串大写为:",s.upper())
